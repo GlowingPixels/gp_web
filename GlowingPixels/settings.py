3df226c941cdd79398fc6749e96f42715c892b30
Ritankar <ritankarpaul47@gmail.com>
2018-12-26 20:56:04 +0530

    html css sorted

--- GlowingPixels/settings.py
+++ GlowingPixels/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'gallery',
     'stdimage',
-    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,7 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
     path('<str:category>/image360/', views.gallery360, name='gallery360'),
     path('<str:category>/', views.gallery, name='gallery'),
