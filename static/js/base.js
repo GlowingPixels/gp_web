a3d9b1957b21f6284dcee792a9eaeb7ec33faec2
Kripa Sindhu Bairagi <kripabairagi0047@gmail.com>
2019-01-02 10:43:12 +0530

    changes made

--- static/js/base.js
+++ static/js/base.js
@@ -14,22 +14,35 @@ $(function(){
 
 // code for changing color for each letter of navbar-brand
 
-//var brandname = $("#colorchange").text();
-// $(".navbar-brand").hover(()=>{
-// 	for(var i=1; i<=13; i++){
-// 		var id = "#let" + String(i);
-// 		var randcolor = randomcolor();
-// 		$(id).css('color', String(randcolor));
-// 		// $(id).css({textShadow: String(randcolor) + " 0 0 3px"});
-// 		console.log("count");
-// 	}
-// });
-
-// Code for changing color of hole navbar-brand
 $(".navbar-brand").hover(()=>{
 	var randcolor = randomcolor();
-	$(".navbar-brand").css('color', String(randcolor));
+	var time = 0;
+	for(var i=1; i<=13; i++){
+		var id = "#let" + String(i);
+		time = time +170;
+		$(id).css('transition',String(time) + "ms");
+		$(id).css('color', String(randcolor));
+		// $(id).css({textShadow: String(randcolor) + " 0 0 3px"});
+	}
 });
+
+// Code for changing color of whole navbar-brand
+
+// $(".navbar-brand").hover(()=>{
+// 	var randcolor = randomcolor();
+// 	$(".navbar-brand").css('color', String(randcolor));
+// });
+
+// code for changing color of catagory
+
+// $(".dropdown").hover(()=>{
+// 	var randcolor2 = randomcolor();
+// 	$("#cato").css('color', String(randcolor2));
+// 	$("#underln").css('border-bottom',"3px solid " + String(randcolor2));
+// },()=>{
+// 	$("#cato").css('color', "black");
+// 	$("#underln").css('border-bottom',"3px solid black");
+// });
 
 
 
