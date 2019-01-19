b8333f5fdb94f2f3ce161578d7638a54b77c5268
Kripa Sindhu Bairagi <kripabairagi0047@gmail.com>
2019-01-19 23:40:34 +0530

    Debug Now

---
+++ static/js/profile.js
@@ -0,0 +1,10 @@
+console.log("profile.js connected");
+
+// settings hover effects
+$(".fa-cog").hover(
+    ()=>{
+        $(".fa-cog").css('transform', 'rotate(90deg)');
+    },()=>{
+        $(".fa-cog").css('transform', 'rotate(0deg)');
+    }
+);
