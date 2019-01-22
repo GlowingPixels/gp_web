1fbe2541ec2f66118522e4d5dffeed0d77329833
Ritankar <ritankarpaul47@gmail.com>
2019-01-22 20:47:19 +0530

    User changeform added

--- users/models.py
+++ users/models.py
@@ -6,4 +6,6 @@ class CustomUser(AbstractUser):
 
     profile_pic = StdImageField(upload_to="profile_pic/", blank=True)
     cover_pic = StdImageField(upload_to="cover_pic/", blank=True)
+    facebook_link = models.URLField(max_length=200, blank=True)
+    instagram_link = models.URLField(max_length=200, blank=True)
     
