ca300926f821df5ea6e1315996fec51eeb739560
Kripa Sindhu Bairagi <kripabairagi0047@gmail.com>
2019-01-24 19:58:27 +0530

    HTML added

--- static/js/profile.js
+++ static/js/profile.js
@@ -12,3 +12,18 @@ $(".fa-cog").hover(
 $(".fa-pencil-alt").click(function () {
     $(".picEdit").fadeToggle();
 });
+
+//code for color changing with time interval
+setInterval(function(){
+    var randcolor = randomcolor();
+    $(".fa-plus").css("color", String(randcolor));
+    $(".contributeArea").css("box-shadow", "0 0 1px " +  String(randcolor));
+}, 3000);
+
+// random color generator
+function randomcolor()  {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgba("+r+", "+g+", "+b+")"
+}
