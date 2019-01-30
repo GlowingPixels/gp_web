61f850d6e0421dc43c42e56632217db0e438da05
Kripa Sindhu Bairagi <kripabairagi0047@gmail.com>
2019-01-30 21:56:58 +0530

    html added

--- static/js/base_homepage.js
+++ static/js/base_homepage.js
@@ -9,3 +9,20 @@ $(".screen_side").click(function(){
     $(".screen_side").fadeOut();
     $(".category_side").css("left", "-80%");
 });
+
+// code for color toggle on clicking list_items
+$(".list_items").click(function(){
+    $(this).children("span").toggleClass("change_color");
+});
+
+// code for showing and unshowing sub-category
+$(".list_items").click(function(){
+    $(this).children(".sub_category").slideToggle();
+});
+
+// code for footer
+$(".details_sections_col").children("ul").children("li").children("a").hover(function(){
+    $(this).children("i").css("opacity", "1");
+},function(){
+    $(this).children("i").css("opacity", "0");
+});
